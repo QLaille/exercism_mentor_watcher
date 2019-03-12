746778e4dd3373a6fade91144b6baedc5bddfdd5
Quentin Laillé <quentin.laille@epitech.eu>
2019-03-12 14:04:28 +0100

    added the dockerfile from exercism_docker_c and modified the script to be able to be ran from every possible configuration

--- exercism_mentor_watcher.sh
+++ exercism_mentor_watcher.sh
@@ -1,12 +1,36 @@
 #!/bin/bash
 
+#Assuming the dockerfile is in the same dir as the script
+#Courtesy of : https://stackoverflow.com/a/630645/7235297
+function getScriptPath() {
+	prg=$0
+
+	if [ ! -e "$prg" ]; then
+	  case $prg in
+	    (*/*) exit 1;;
+	    (*) prg=$(command -v -- "$prg") || exit;;
+	  esac
+	fi
+
+	dir=$(
+	  cd -P -- "$(dirname -- "$prg")" && pwd -P
+	) || exit
+	prg=$dir/$(basename -- "$prg") || exit 
+}
+
+# Run docker build in a new terminal
+function dockerRun() {
+	getScriptPath
+	command="sudo docker build --build-arg token=$EXERCISM_MENTOR_TOKEN --build-arg trackId=$uuid $dir --no-cache"
+	gnome-terminal -x sh -c "printf \"\e]2;EXERCISM\a\";$command; bash"
+}
+
 while true
 do
 CLIPBOARD=$(xsel)
 	if [[ $CLIPBOARD =~ "exercism download --uuid="[a-zA-Z0-9]+ ]]; then
 		uuid=${CLIPBOARD#"exercism download --uuid="}
-		echo $uuid
-		gnome-terminal -x sh -c "printf \"\e]2;EXERCISM\a\";mentor_track.sh $uuid; bash"
+		dockerRun
 		$(xsel -c)
 	fi
 sleep 2
